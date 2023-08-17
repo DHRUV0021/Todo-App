4b6f2fb0f5862982070477035b20e9716997b293
Gorasiya Dhruv <gorasiyadhruv021@gmail.com>
2023-08-17 11:29:42 +0530

    some changes

--- src/app/environment/config.ts
+++ src/app/environment/config.ts
@@ -1,4 +1,4 @@
 export const enviroment ={
-    toAPI:"http://10.10.4.2:16100/todo" //Server URL
-    // toAPI:"http://localhost:3000/Todos" //Server URL
+    toAPI:"http://10.10.3.188:16100/todo" //Server URL
+    // toAPI:"http://localhost:3000/Todos" //Json URL
 }
